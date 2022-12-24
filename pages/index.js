0d5d4ca1538c08582d49616afb512f9083183304
agarwalchirag19 <botxchirag@gmail.com>
2022-12-24 10:17:44 +0530

    drfgnrnrs

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div className="flex flex-row items-center justify-center min-h-screen py-2">
+      <head>
+        <title>create next app</title>
+        <link rel="icon" href ="/favicon.ico" />
+      </head>
+      <h1>this is insta clone</h1>
+    </div>
+  );
+}
